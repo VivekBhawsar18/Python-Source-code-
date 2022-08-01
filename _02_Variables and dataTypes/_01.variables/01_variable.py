859ec19d8b5280b4cb5063b65273b44f56dfb0c3
Vivek Bhawsar <93441216+VivekBhawsar18@users.noreply.github.com>
2022-08-01 10:50:59 +0530

    Add files via upload

---
+++ _02_Variables and dataTypes/_01.variables/01_variable.py
@@ -0,0 +1,19 @@
+a_122 = '''vivek'''
+# a = 'vivek'
+# a = "vivek"
+b = 345
+c = 45.32
+d = True
+# d = None
+
+# Printing the variables
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Printing the type of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
